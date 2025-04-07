c4b749e8b1c4dbbf6a8d1a038e4370bc339e4f19
mastergroup <juli_tepa@hotmail.com>
2025-04-07 01:38:40 -0300

    Agrego Flask al requirements.txt

--- app.py
+++ app.py
@@ -224,6 +224,6 @@ def page_not_found(e):
 def internal_server_error(e):
     return render_template('500.html'), 500
 
-if __name__ == "__main__":
-    init_db()
-    app.run(debug=True)
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
